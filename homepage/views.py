7c610ec5627fa3793d13c7ed51741141122da040
sanjay <sanjaypython22@gmail.com>
2025-08-02 22:35:42 +0530

    p

--- homepage/views.py
+++ homepage/views.py
@@ -1,6 +1,12 @@
 from django.shortcuts import render
 
+from news.models import News
+
 
 # Create your views here.
 def homepage(request):
-    return render(request, 'index.html')
+    crime = News.objects.filter(category__name='Crime')
+    context = {
+        'crime': crime,
+    }
+    return render(request, 'index.html', context)
