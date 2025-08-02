b94fe7656ba62101f35b6262919ae056db3b4b99
sanjay <sanjaypython22@gmail.com>
2025-08-02 21:31:02 +0530

    p

--- homepage/views.py
+++ homepage/views.py
@@ -3,4 +3,6 @@ from django.shortcuts import render
 
 # Create your views here.
 def homepage(request):
-    return render(request, 'index.html')
+    # return render(request, 'index.html')
+    # return render(request, 'index1.html')
+    return render(request, 'index2.html')
