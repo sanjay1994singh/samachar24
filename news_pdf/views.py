db8b65d2ca14937175b6b661ac240400f5e42d08
sanjay <sanjaypython22@gmail.com>
2025-08-05 10:15:46 +0530

    p

--- news_pdf/views.py
+++ news_pdf/views.py
@@ -5,5 +5,4 @@ from .models import NewsPDF
 # Create your views here.
 def news_pdf(request):
     latest_pdf = NewsPDF.objects.last()
-    print(latest_pdf.pdf_file, '==========================latest_pdf')
     return render(request, 'news_pdf.html', {'pdf': latest_pdf})
